1400e38e6a20aa614b82c3992958e7a924055d3e
sschink <89204055+sschink@users.noreply.github.com>
2022-01-28 10:57:23 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 import os 
 import datetime
-path = 'D:\Demo-Project01\Source\Test'
+import glob
+path = 'D:\Demo-Project01\Test'
 count=0
 count1=0
 file=[]
@@ -8,9 +9,9 @@ for files in os.listdir(path):
     count+=1
     file.append(files)
 print(os.path.join(path),f" \n List of Files in Current Directory are : {file}\n The no of files present in current Directory are : {count}")
-#older_files=[]
-#remove_list=[]
-#no_of_olddays=[]
+older_files=[]
+remove_list=[]
+no_of_olddays=[]
 today = datetime.datetime.today()
 os.chdir(path)
 for root,directories,files in os.walk(path,topdown=False):
@@ -20,9 +21,10 @@ for root,directories,files in os.walk(path,topdown=False):
         
         if filetime.days>=7:
             count1+=1
-            print("The no of old files are : {count1}",os.path.join(root, name), filetime.days)
+            print(os.path.join(root, name), filetime.days)
             os.remove(os.path.join(root, name))
             
-for dirpath,filenames in os.walk(path):
+for dirpath,dirnames,filenames in os.walk(path):
      print("Current Path",dirpath)
-     print("The Updated Files are:",filenames)  
+     print("current Directories",dirnames)
+     print("The Updated Files are:",filenames)      
