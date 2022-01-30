2df96b7d16896a11e5822aa910814a66ed23e1ce
sschink <89204055+sschink@users.noreply.github.com>
2022-01-30 23:57:42 +0530

    Create ZipFile.py

---
+++ ZipFile.py
@@ -0,0 +1,41 @@
+import os
+import time
+#from pathlib import Path
+import zipfile
+
+dir_name = 'G:\Python_Workspace\Flask_pytest\Test_zip'
+
+list_of_files = filter( lambda x: os.path.isfile(os.path.join(dir_name, x)),
+                        os.listdir(dir_name) )
+
+list_of_files = sorted( list_of_files,
+                        key = lambda x: os.path.getmtime(os.path.join(dir_name, x)))
+
+for file in list_of_files[0:-5]:
+    os.remove(os.path.join(dir_name,file))
+list_of_files.reverse()
+print("The Update Files are:")
+for file_name in list_of_files[:5]:
+    file_path = os.path.join(dir_name, file_name)
+    timestamp_str = time.strftime('%d/%m/%Y :: %H:%M:%S',
+                                time.gmtime(os.path.getmtime(file_path)))
+    print(timestamp_str, ' -->', file_name)
+
+
+def prepare_zip(dir_name):
+    new_file = dir_name + '.zip'
+    # creating zip file with write mode
+    zip = zipfile.ZipFile(new_file, 'w', zipfile.ZIP_DEFLATED)
+    # Walk through the files in a directory
+    for dir_name, dir_names, files in os.walk(dir_name):
+        f_path = dir_name.replace(dir_name, '')
+        f_path = f_path and f_path + os.sep
+        # Writing each file into the zip
+        for file in files:
+            zip.write(os.path.join(dir_name, file), f_path + file)
+    zip.close()
+    print("File Created successfully..")
+    return new_file
+
+
+prepare_zip(dir_name)
