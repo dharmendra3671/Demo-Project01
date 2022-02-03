ef196260191a5205e734f67e9cedb883d0cd5f2b
sschink <89204055+sschink@users.noreply.github.com>
2022-02-03 12:07:20 +0530

    Update PythonwithJson.py

--- PythonwithJson.py
+++ PythonwithJson.py
@@ -38,7 +38,6 @@ try:
                 f_path = dir_file.replace(dir_file, '')
                 f_path = f_path and f_path + os.sep
                 
-                
                 for file in files:
                     zip.write(os.path.join(filedata['path'], file), f_path + file)
             zip.close()
