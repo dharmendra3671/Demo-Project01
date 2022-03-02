43ac0a60a08c6d9ab588db117703a86d4b7c086c
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-02 12:04:09 +0530

    Update artifactory.py

--- artifactory.py
+++ artifactory.py
@@ -1,9 +1,18 @@
+import os
+import glob
+
 from artifactory import ArtifactoryPath
-path = ArtifactoryPath('http://127.0.0.1:8082/artifactory',
-    auth=('admin', 'Kumar@6805'))
+path = ArtifactoryPath('http://127.0.0.1:8082/artifactory/python',
+    auth=('admin', 'Root@123'))
 path.touch()
 
-path.mkdir('Zipfile_Artifactory')
+def filename():
+    os.chdir(r'D:\Test\t1')
+    for file in glob.glob("*.zip"):
+        if os.path.exists(file):
+            return file
 
-path.deploy_file('D:\\Test\\*.zip')
+
+path.deploy_file(filename())
+
                 
